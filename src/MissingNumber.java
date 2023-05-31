cc40bbfa1df9d27ead3e0e6161b52db2074c4058
Aniket Mukherjee <aniket13.am@gmail.com>
2023-05-31 20:02:37 +0530

    added xor part

--- src/MissingNumber.java
+++ src/MissingNumber.java
@@ -15,7 +15,7 @@ public class MissingNumber {
 
     // lets try a different approach subtract between (summation of First N natural
     // numbers) and (summation of all array elements)
-    // the below code will only work if the numbers are all distinct i.e every
+    // the below function will only work if the numbers are all distinct i.e every
     // number appearing once only!
     private static int missingNumber(int[] arr) {
         int sum = 0;
@@ -23,6 +23,18 @@ public class MissingNumber {
             sum += arr[i];
         }
         return (arr.length * (arr.length + 1) / 2) - sum;
+    }
+
+    // Another good solution is to use bitwise xor, iff the numbers are all distinct
+    private static int missing(int[] arr) {
+        int xor_1 = 0;
+        for (int i = 1; i <= arr.length; i++)
+            xor_1 ^= i;
+        int xor_2 = 0;
+        for (int i = 0; i < arr.length; i++) {
+            xor_2 ^= arr[i];
+        }
+        return xor_1 ^ xor_2;
     }
 
     public static void main(String[] args) {
@@ -35,6 +47,8 @@ public class MissingNumber {
         }
         System.out.println(Arrays.toString(arr));
         System.out.println("The missing number from the array is : " + missingNumber(arr, N));
-        System.out.println("The missing number from the array is : " + missingNumber(arr));
+        // System.out.println("The missing number from the array is : " +
+        // missingNumber(arr));
+        // System.out.println("The missing number from the array is : " + missing(arr));
     }
 }
